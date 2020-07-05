221216579f357c9015602d507003bd568dad7bef
The-element <nicke.jonsson@live.com>
2020-07-05 09:02:01 +0000

    added js to header and some add. styling

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -4,9 +4,15 @@ window.onscroll = function() {scrollFunction()};
 function scrollFunction() {
   if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
     document.getElementById("header").style.background = "black";
+    document.getElementById("header").style.height = "40px";
+    document.getElementById("logo-header").style.height = "20px";
+    document.getElementById("logo-header").style.marginTop = "-20px";
     document.getElementById("header").style.fontSize = "14px";
   } else {
     document.getElementById("header").style.background = "transparent";
+    document.getElementById("header").style.height = "60px";
+    document.getElementById("logo-header").style.height = "40px";
+    document.getElementById("logo-header").style.marginTop = "-40px";
     document.getElementById("header").style.fontSize = "16px";
   }
 }
