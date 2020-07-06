705f5c5b5785f4d9170e5b1d1c3a0e87a71b08e5
The-element <nicke.jonsson@live.com>
2020-07-06 13:12:43 +0000

    adjusted some css and js to make navbar prettier

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -7,21 +7,21 @@ function scrollFunction() {
         if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
             document.getElementById("header").style.background = "black";
             document.getElementById("header").style.height = "50px";
-            document.getElementById("logo-header").style.height = "30px";
-            document.getElementById("logo-header").style.marginTop = "-35px";
+            //document.getElementById("logo-header").style.height = "30px";
+            //document.getElementById("logo-header").style.paddingTop = "-35px";
             document.getElementById("header").style.fontSize = "14px";
         } else {
             document.getElementById("header").style.background = "transparent";
             document.getElementById("header").style.height = "60px";
-            document.getElementById("logo-header").style.height = "40px";
-            document.getElementById("logo-header").style.marginTop = "-10px";
+            //document.getElementById("logo-header").style.height = "40px";
+            //document.getElementById("logo-header").style.paddingTop = "-10px";
             document.getElementById("header").style.fontSize = "16px";
         }
     } else {
             document.getElementById("header").style.background = "black";
             document.getElementById("header").style.height = "60px";
-            document.getElementById("logo-header").style.height = "40px";
-            document.getElementById("logo-header").style.marginTop = "-10px";
+            //document.getElementById("logo-header").style.height = "40px";
+            //document.getElementById("logo-header").style.paddingTop = "-10px";
             document.getElementById("header").style.fontSize = "16px";
             document.getElementsByClassName("nav-item").style.background = "black";
     }
