6e8a4626f282182187e4f6716774edacdde1a8a8
The-element <nicke.jonsson@live.com>
2020-07-08 13:37:27 +0000

    changed the animation of the progress bars

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -1,5 +1,7 @@
 // When the user scrolls down 50px from the top of the document, resize the header's font size
-window.onscroll = function() {scrollFunction()};
+window.onscroll = function() {
+    scrollFunction()
+};
 var screenSize = parseInt(window.screen.width);
 
 function scrollFunction() {
@@ -27,22 +29,29 @@ function scrollFunction() {
     }
 }
 
+window.scroll(function(){
+    var hT = $(".skills-header").offset().top,
+        hH = $(".skills-header").outerHeight(),
+        wH = $(window).height(),
+        wS = $(this).scrollTop();
+    if(wS > (hT+hH-wH)){
+        myTimer();
+    }
+})
 
 
 var myVar=setInterval(function(){myTimer()},1);
-var count = 0;
-function myTimer() {
-if(count < 50){
-  $('.progress').css('width', count + "%");
-  count += 0.05;
-   document.getElementById("demo").innerHTML = Math.round(count) +"%";
-   // code to do when loading
-  }
-  
-  else if(count > 99){  
-      // code to do after loading
-  $('.progress').css('width', count + "%");
-  count = 0;
-  
-  }
+var count = 30;
+
+function myTimer(bars, listOfPer) {
+    if(count < 50){
+        $('.progress1').css('width', count + "%");
+        count += 0.5;
+        document.getElementById("demo").innerHTML = Math.round(count) +"%";
+    }
+    if(count > 99){
+        $('.progress2').css('width', count + "%");
+        count += 0.5;
+        document.getElementById("demo2").innerHTML = Math.round(count) +"%";
+    }
 }
